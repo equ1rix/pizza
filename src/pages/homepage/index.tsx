e89178de3f47cb4b8604cdf314b17983392150fe
equ1rix <103569669+equ1rix@users.noreply.github.com>
2024-05-03 23:39:54 +0200

    task(componenst): add modal with info

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -6,11 +6,18 @@ import { selectPizza } from "../../redux/selector";
 import { LogoIcon } from "../../components/iconbuttons/icons";
 import Header from "../../components/header";
 import PizzaCards from "../../components/pizzacards";
+import Modal from "../../components/modal";
+import { useState } from "react";
 
 const Homepage = () => {
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(true);
   const state = useSelector(selectPizza);
   const quantity = state.reduce((total, pizza) => total + pizza.quantity, 0);
   const price = state.reduce((total, pizza) => total + pizza.totalPrice, 0);
+
+  const closeModalHandler = () => {
+    setIsOpenModal(false);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 p-4">
@@ -25,6 +32,12 @@ const Homepage = () => {
           className="h-screen overflow-y-auto"
           style={{ maxHeight: `calc(100% - ${100}px)` }}
         >
+          {isOpenModal && (
+            <Modal
+              onClose={closeModalHandler}
+              title="Information regarding the project"
+            />
+          )}
           <PizzaCards />
         </div>
       </div>
